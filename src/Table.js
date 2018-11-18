af2b842bbddefaec38ce7ba99d7709dc52888e09
John Flynn <jflynnpics@gmail.com>
2018-11-18 18:19:03 -0500

    adjust json

--- src/Table.js
+++ src/Table.js
@@ -1,107 +1,110 @@
 import React, { Component } from 'react'
 import TableCell from './TableCell'
+import MinesweeperData from './minesweeperboard.json'
 class Table extends Component {
   constructor(props) {
-    super(props) 
-      this.state = {
-        status: ''
-      }
+    super(props)
+    this.state = {
+      status: ''
     }
-  refreshBoard = boardID => {
-
   }
-  boardID=[this.row],[this.column]
-  url = 'https://minesweeper-api.herokuapp.com/games'
-  axios.get(url).then(response => {this.setState({
-    
-  })})
-}
-  let
 
-  newGame ()
+  
+    boardID
+    let url = 'https://minesweeper-api.herokuapp.com/games'
+    axios.get(url).then(response => {this.setState({
+
+    })})
+  
+    let
+
+
   render() {
     return (
-      // <button onClick={this.state.newGame()}></button>
+      <button onClick={this.state.newGame()}></button>
       <table className="board">
         <tbody>
           <tr>
-            <TableCell  />
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
+            <TableCell status={this.props.board[0][0]} />
+            <TableCell status={this.props.board[0][1]} />
+            <TableCell status={this.props.board[0][2]} />
+            <TableCell status={this.props.board[0][3]} />
+            <TableCell status={this.props.board[0][4]} />
+            <TableCell status={this.props.board[0][5]} />
+            <TableCell status={this.props.board[0][6]} />
+            <TableCell status={this.props.board[0][7]} />
           </tr>
           <tr>
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
+            <TableCell status={this.props.board[1][0]} />
+            <TableCell status={this.props.board[1][1]} />
+            <TableCell status={this.props.board[1][2]} />
+            <TableCell status={this.props.board[1][3]} />
+            <TableCell status={this.props.board[1][4]} />
+            <TableCell status={this.props.board[1][5]} />
+            <TableCell status={this.props.board[1][6]} />
+            <TableCell status={this.props.board[1][7]} />
           </tr>
           <tr>
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
+            <TableCell status={this.props.board[2][0]} />
+            <TableCell status={this.props.board[2][1]} />
+            <TableCell status={this.props.board[2][2]} />
+            <TableCell status={this.props.board[2][3]} />
+            <TableCell status={this.props.board[2][4]} />
+            <TableCell status={this.props.board[2][5]} />
+            <TableCell status={this.props.board[2][6]} />
+            <TableCell status={this.props.board[2][7]} />
           </tr>
 
           <tr>
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
+            <TableCell status={this.props.board[3][0]} />
+            <TableCell status={this.props.board[3][1]} />
+            <TableCell status={this.props.board[3][2]} />
+            <TableCell status={this.props.board[3][3]} />
+            <TableCell status={this.props.board[3][4]} />
+            <TableCell status={this.props.board[3][5]} />
+            <TableCell status={this.props.board[3][6]} />
+            <TableCell status={this.props.board[3][7]} />
           </tr>
 
           <tr>
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
+            <TableCell status={this.props.board[4][0]} />
+            <TableCell status={this.props.board[4][1]} />
+            <TableCell status={this.props.board[4][2]} />
+            <TableCell status={this.props.board[4][3]} />
+            <TableCell status={this.props.board[4][4]} />
+            <TableCell status={this.props.board[4][5]} />
+            <TableCell status={this.props.board[4][6]} />
+            <TableCell status={this.props.board[4][7]} />
           </tr>
           <tr>
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
+            <TableCell status={this.props.board[5][0]} />
+            <TableCell status={this.props.board[5][1]} />
+            <TableCell status={this.props.board[5][2]} />
+            <TableCell status={this.props.board[5][3]} />
+            <TableCell status={this.props.board[5][4]} />
+            <TableCell status={this.props.board[5][5]} />
+            <TableCell status={this.props.board[5][6]} />
+            <TableCell status={this.props.board[5][7]} />
           </tr>
           <tr>
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
-            <TableCell />
+            <TableCell status={this.props.board[6][0]} />
+            <TableCell status={this.props.board[6][1]} />
+            <TableCell status={this.props.board[6][2]} />
+            <TableCell status={this.props.board[6][3]} />
+            <TableCell status={this.props.board[6][4]} />
+            <TableCell status={this.props.board[6][5]} />
+            <TableCell status={this.props.board[6][6]} />
+            <TableCell status={this.props.board[6][7]} />
+          </tr>
+          <tr>
+            <TableCell status={this.props.board[7][0]} />
+            <TableCell status={this.props.board[7][1]} />
+            <TableCell status={this.props.board[7][2]} />
+            <TableCell status={this.props.board[7][3]} />
+            <TableCell status={this.props.board[7][4]} />
+            <TableCell status={this.props.board[7][5]} />
+            <TableCell status={this.props.board[7][6]} />
+            <TableCell status={this.props.board[7][7]} />
           </tr>
         </tbody>
       </table>
